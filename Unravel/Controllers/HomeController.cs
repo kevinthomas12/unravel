55371b1647fb05a6e5f3fa4af5a8ebd82744d971
kevinthomas12 <kevinthomas12@hotmail.com>
2014-05-08 22:34:04 +0100

    BootStrap And Big Video Awaiting Upload To Vimeo

--- Unravel/Controllers/HomeController.cs
+++ Unravel/Controllers/HomeController.cs
@@ -3,30 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
 
-namespace Unravel.Controllers
+namespace BootstrapMvcSample.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BootstrapBaseController
     {
+        private static List<HomeInputModel> _models = ModelIntializer.CreateHomeInputModels();
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
+           
+            var homeInputModels = _models;                                      
+            return View(homeInputModels);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(HomeInputModel model)
         {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
+            if (ModelState.IsValid)
+            {
+                model.Id = _models.Count==0?1:_models.Select(x => x.Id).Max() + 1;
+                _models.Add(model);
+                Success("Your information was saved!");
+                return RedirectToAction("Index");
+            }
+            Error("there were some errors in your form.");
+            return View(model);
         }
 
-        public ActionResult Contact()
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(new HomeInputModel());
         }
+
+        public ActionResult Delete(int id)
+        {
+            _models.Remove(_models.Get(id));
+            Information("Your widget was deleted");
+            if(_models.Count==0)
+            {
+                Attention("You have deleted all the models! Create a new one to continue the demo.");
+            }
+            return RedirectToAction("index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = _models.Get(id);
+            return View("Create", model);
+        }
+        [HttpPost]        
+        public ActionResult Edit(HomeInputModel model,int id)
+        {
+            if(ModelState.IsValid)
+            {
+                _models.Remove(_models.Get(id));
+                model.Id = id;
+                _models.Add(model);
+                Success("The model was updated!");
+                return RedirectToAction("index");
+            }
+            return View("Create", model);
+        }
+
+		public ActionResult Details(int id)
+        {
+            var model = _models.Get(id);
+            return View(model);
+        }
+
     }
 }
